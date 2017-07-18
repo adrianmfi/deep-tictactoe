c0063835a6996e786e5d14d26dc87ebdbb6bd30b
Adrian Meidell Fiorito <adrianmefi@gmail.com>
2017-07-18 23:58:08 +0200

    refactor generate_data

--- src/image/conv_net/dataset/generate_data.py
+++ src/image/conv_net/dataset/generate_data.py
@@ -3,28 +3,37 @@ from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 
-# Input args
-img_width = 64
-img_height = 64
-font_size = 50
-font_intensity = 0
-background_color = 255
-draw_string = 'Test'
-draw_xy = (0, 0)
-img_fname = 'test.png'
-font_fname = 'OpenSans-Regular.ttf'
-font_folder = 'fonts'  # Relative to this file
-data_folder = 'data'  # Relative to this file
-# End input args
 
-dir = os.path.dirname(__file__)
-font_path = os.path.join(dir, font_folder, font_fname)
-img_path = os.path.join(dir, data_folder, img_fname)
+def make_PIL_img_with_text(text_str, text_size, text_pos, text_intensity,
+                           img_size, bg_intensity, font_path):
+    font = ImageFont.truetype(font_path, text_size)
+    img = Image.new('L', img_size, bg_intensity)
+    draw = ImageDraw.Draw(img)
+    draw.text(text_pos, text_str, text_intensity, font=font)
+    return img
 
-font = ImageFont.truetype(font_path, font_size)
-img = Image.new('L', (img_width, img_height), background_color)
-draw = ImageDraw.Draw(img)
-draw.text(draw_xy, draw_string, font_intensity, font=font)
-draw = ImageDraw.Draw(img)
-draw = ImageDraw.Draw(img)
-img.save(img_path)
+
+def main():
+    text_str = 'x'
+    text_size = 50
+    text_pos = (0, 0)
+    text_intensity = 0
+    img_size = (60, 60)
+    bg_intensity = 255
+    img_fname = 'test.png'
+    font_fname = 'OpenSans-BoldItalic.ttf'
+    font_folder = 'fonts'  # Relative to this file
+    data_folder = ''  # Relative to this file
+
+    dir = os.path.dirname(__file__)
+    font_path = os.path.join(dir, font_folder, font_fname)
+
+    img = make_PIL_img_with_text(text_str, text_size, text_pos,
+                                 text_intensity, img_size, bg_intensity, font_path)
+
+    img_path = os.path.join(dir, data_folder, img_fname)
+    img.save(img_path)
+
+
+if __name__ == '__main__':
+    main()
